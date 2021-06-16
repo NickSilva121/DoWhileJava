955342e662e1f2450ec5fc71bfde3f74ab33273c
Nicolas Pereira <77083485+NickSilva121@users.noreply.github.com>
2021-06-16 05:43:40 -0300

    Update DoWhile.java

--- DoWhile.java
+++ DoWhile.java
@@ -1,21 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dowhile;
 
-/**
- *
- * @author linuxlite
- */
+import java.util.Locale;
+import java.util.Scanner;
+
 public class DoWhile {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        char resp;
+        
+        do {
+            System.out.print("Digite a temperatura em Celsius: ");
+            double C = sc.nextDouble();
+            double F = 9.0 * C / 5.0 + 32;
+            System.out.printf("Equivalente em Fahrenheit: %.2f\n", F);
+            System.out.println("Deseja repetir (s/n)?");
+            resp = sc.next().charAt(0);
+        }while(resp != 'n');
+        
+        sc.close();
     }
-    
 }
